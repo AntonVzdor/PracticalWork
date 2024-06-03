7e8477d8bcb9b7abd76beb74b13f9dd4e1077b60
AntonVzdor <der_nom@mail.ru>
2024-06-03 18:48:40 +0300

    первая практическая готова

--- app/src/main/java/com/example/practicalwork/MainActivity.kt
+++ app/src/main/java/com/example/practicalwork/MainActivity.kt
@@ -4,25 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign.Companion.Justify
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practicalwork.ui.theme.PracticalWorkTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PracticalWorkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    PracticalWorkOne()
                 }
             }
         }
@@ -30,17 +40,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    Text(
-        text = "Start",
+fun PracticalWorkOne(
+    modifier: Modifier = Modifier
+){
+    Column(
         modifier = modifier
-    )
+    ) {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null,
+            modifier = modifier
+                .fillMaxWidth())
+        Text(
+            text = stringResource(
+                R.string.text_title
+            ),
+            fontSize = 24.sp,
+            modifier = modifier
+                .padding(all = 16.dp)
+        )
+        Text(
+            text = stringResource(
+                R.string.text_first
+            ),
+            textAlign = Justify,
+            modifier = modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = stringResource(
+                R.string.text_second
+            ),
+            textAlign = Justify,
+            modifier = modifier
+                .padding(all = 16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PracticalWorkPreview() {
     PracticalWorkTheme {
-        Greeting()
+        PracticalWorkOne()
     }
 }
