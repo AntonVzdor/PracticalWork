c1ee0e5ad1c14e58cf542a02e590d7a1490389e5
dernom <der_nom@mail.ru>
2024-06-13 16:55:47 +0300

    в процессе

--- app/src/main/java/com/example/practicalwork/MainActivity.kt
+++ app/src/main/java/com/example/practicalwork/MainActivity.kt
@@ -4,12 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.practicalwork.ui.theme.PracticalWorkTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,8 +41,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ClickBehavior(modifier: Modifier = Modifier) {
+fun ClickBehavior(){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(15.dp)
+        ){
+        Image(painter = painterResource(R.drawable.lemon_tree),
+             contentDescription = null,
+            modifier = Modifier
 
+        )}
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(stringResource(R.string.LemonTree))
+    }
 }
 
 @Preview(showBackground = true)
