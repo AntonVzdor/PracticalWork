1a3ef15f67312aeab8c209966b45dfe300a6c856
dernom <der_nom@mail.ru>
2024-06-25 17:39:16 +0300

    шестая практическая готова

--- app/src/main/java/com/example/practicalwork/MainActivity.kt
+++ app/src/main/java/com/example/practicalwork/MainActivity.kt
@@ -4,19 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,12 +28,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practicalwork.ui.theme.PracticalWorkTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,7 +60,6 @@ fun ArtSpaceScreen(){
 
     var imageNextAndPast by remember { mutableIntStateOf(1) }
 
-    Surface(modifier = Modifier) {
         when (imageNextAndPast) {
             1 -> ArtSpace(
                 image = painterResource(R.drawable.image_1),
@@ -90,7 +93,7 @@ fun ArtSpaceScreen(){
                 imageClickPast = { imageNextAndPast = 3 }
             )
         }
-    }
+
 }
 
 @Composable
@@ -101,40 +104,79 @@ fun ArtSpace(
     imageClickNext: () -> Unit,
     imageClickPast: () -> Unit
 ) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Image(
-            painter = image,
-            contentDescription = null,
-            modifier = Modifier
-                .padding(all = 25.dp)
-                .size(450.dp)
-                .border(
-                    BorderStroke(2.dp, Color.Gray)
-                ))
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+                Image(
+                    painter = image,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(start = 40.dp, end = 40.dp)
+                        .shadow(3.dp)
+                        .padding(top = 45.dp, bottom = 45.dp)
+                        .size(365.dp)
+                )
 
-        Text(text = title)
+            Spacer(modifier = Modifier.height(20.dp))
 
-        Spacer(modifier = Modifier.height(5.dp))
+            Column(modifier = Modifier
+                .fillMaxWidth()
 
-        Text(text = text)
+                .padding(start = 40.dp, end = 40.dp)
+                .shadow(1.dp)
+                .background(color = Color.LightGray)
+                .padding(top = 10.dp, bottom = 10.dp)
 
-        Spacer(modifier = Modifier.height(25.dp))
+            ) {
+                Text(
+                    text = title,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(10.dp, 1.dp))
 
-        Row {
-            Button(onClick = imageClickPast) {
-                Text(text = "Past")
+                Spacer(modifier = Modifier.height(5.dp))
+
+                Text(
+                    text = text,
+                    fontSize = 13.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(10.dp, 1.dp)
+                )
             }
-            Spacer(Modifier.width(130.0.dp))
-            Button(onClick = imageClickNext) {
-                Text(text = "Next")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Row {
+                Button(
+                    onClick = imageClickPast,
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color(0xff004D40),
+                        containerColor = Color(0xff9ed6df)
+                    )
+                ) {
+                    Text(
+                        text = "Past",
+                        modifier = Modifier.padding(20.dp,1.dp))
+                }
+
+                Spacer(Modifier.width(90.0.dp))
+
+                Button(
+                    onClick = imageClickNext,
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color(0xff004D40),
+                        containerColor = Color(0xff9ed6df)
+                    )
+                ) { Text(
+                    text = "Next",
+                    modifier = Modifier.padding(20.dp,1.dp))
+                }
             }
         }
-    }
-}
+        }
+
 
 @Preview(showBackground = true)
 @Composable
