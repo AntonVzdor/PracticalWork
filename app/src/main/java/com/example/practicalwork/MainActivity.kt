7c418474e3f19229ee6c4e4e12d02447eb5efdd4
dernom <der_nom@mail.ru>
2024-06-26 14:50:28 +0300

    пятая практическая готова

--- app/src/main/java/com/example/practicalwork/MainActivity.kt
+++ app/src/main/java/com/example/practicalwork/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.practicalwork
 
 import android.os.Bundle
@@ -10,11 +12,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -22,9 +29,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.practicalwork.ui.theme.PracticalWorkTheme
@@ -45,43 +54,65 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun ClickBehaviorScreen(){
 
     var nextImage by remember { mutableIntStateOf(1) }
     var imageNext by remember { mutableIntStateOf(0) }
 
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                Text(
+                    text = "Lemonade",
+                    fontWeight = FontWeight.Bold
+                )
+                }, colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Yellow
+                )
+            )
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            when (nextImage) {
+                1 -> ClickBehavior(
+                    text = stringResource(R.string.LemonTree),
+                    paint = painterResource(R.drawable.lemon_tree),
+                    imageClick = {
+                        nextImage = 2
+                        imageNext = (2..5).random()
+                    }
+                )
 
-    when(nextImage) {
-        1 -> ClickBehavior(
-            text = stringResource(R.string.LemonTree),
-            paint = painterResource(R.drawable.lemon_tree),
-            imageClick = {
-                nextImage = 2
-                imageNext = (2..5).random()
+                2 -> ClickBehavior(
+                    text = stringResource(R.string.Lemon),
+                    paint = painterResource(R.drawable.lemon_squeeze),
+                    imageClick = {
+                        nextImage = 3
+                    }
+                )
+
+                3 -> ClickBehavior(
+                    text = stringResource(R.string.GlassOfLemonade),
+                    paint = painterResource(R.drawable.lemon_drink),
+                    imageClick = {
+                        nextImage = 4
+                    }
+                )
+
+                4 -> ClickBehavior(
+                    text = stringResource(R.string.EmptyGlass),
+                    paint = painterResource(R.drawable.lemon_restart),
+                    imageClick = {
+                        nextImage = 1
+                    }
+                )
             }
-        )
-        2 -> ClickBehavior(
-            text = stringResource(R.string.Lemon),
-            paint = painterResource(R.drawable.lemon_squeeze),
-            imageClick = {
-                nextImage = 3
-            }
-        )
-        3 -> ClickBehavior(
-            text = stringResource(R.string.GlassOfLemonade),
-            paint = painterResource(R.drawable.lemon_drink),
-            imageClick = {
-                nextImage = 4
-            }
-        )
-        4 -> ClickBehavior(
-            text = stringResource(R.string.EmptyGlass),
-            paint = painterResource(R.drawable.lemon_restart),
-            imageClick = {
-                nextImage = 1
-            }
-        )
+        }
     }
 }
 
