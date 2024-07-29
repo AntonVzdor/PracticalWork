06746d041403bc0ae2951d892235fe63a56f01c8
AntonVzdor <der_nom@mail.ru>
2024-07-29 14:20:34 +0300

    добавлены данные в iu.theme color shape theme type

--- app/src/main/java/com/example/practicalwork/ui/theme/Type.kt
+++ app/src/main/java/com/example/practicalwork/ui/theme/Type.kt
@@ -12,29 +12,28 @@ val Roboto = FontFamily(
     Font(R.font.roboto_regular),
     Font(R.font.roboto_bold)
 )
-// Set of Material typography styles to start with
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    displayLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     )
-    */
 )
