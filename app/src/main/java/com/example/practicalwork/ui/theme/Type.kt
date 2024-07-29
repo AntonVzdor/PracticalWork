7f1b52a660303433c6f7f645d6426dca6c292b39
AntonVzdor <der_nom@mail.ru>
2024-07-29 11:34:53 +0300

    добавления шрифта Roboto

--- app/src/main/java/com/example/practicalwork/ui/theme/Type.kt
+++ app/src/main/java/com/example/practicalwork/ui/theme/Type.kt
@@ -2,10 +2,16 @@ package com.example.practicalwork.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.practicalwork.R
 
+val Roboto = FontFamily(
+    Font(R.font.roboto_regular),
+    Font(R.font.roboto_bold)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
