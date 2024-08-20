420ab4295b44ac9131bd3e29c03f0ac83a3d0381
dernom <der_nom@mail.ru>
2024-08-20 17:28:03 +0300

    почти все сделано осталось только отполировать

--- app/src/main/java/com/example/practicalwork/PhotoCard.kt
+++ app/src/main/java/com/example/practicalwork/PhotoCard.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +22,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -55,6 +58,9 @@ fun PhotoCardItem(
         onClick = {
             expanded = !expanded
         },
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 2.dp
+        ),
         modifier = Modifier.padding(5.dp),
     ) {
             Column(
@@ -68,7 +74,8 @@ fun PhotoCardItem(
                     contentDescription = null,
                     modifier = modifier
                         .fillMaxSize()
-                        .height(200.dp),
+                        .height(200.dp)
+                        .clip(RoundedCornerShape(6.dp)),
                     contentScale = ContentScale.Crop
                 )
 
@@ -76,7 +83,7 @@ fun PhotoCardItem(
                     PhotoAnimate(
                         photo.descriptionPhoto,
                         modifier = Modifier.padding(
-                            start = 16.dp,
+                            start = 2.dp,
                             top = 8.dp,
                             bottom = 16.dp,
                             end = 17.dp
