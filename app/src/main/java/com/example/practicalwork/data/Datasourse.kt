7c87521aa04f68b9dedcfe6e90606c60e8555474
AntonVzdor <der_nom@mail.ru>
2024-10-18 08:16:28 +0300

    Десятая практическая готова

--- app/src/main/java/com/example/practicalwork/data/Datasourse.kt
+++ app/src/main/java/com/example/practicalwork/data/Datasourse.kt
@@ -5,19 +5,19 @@ import com.example.practicalwork.R
 
 object Datasource {
     val dessertList = listOf(
-        Dessert(R.drawable.cupcake, 5, 0),
-        Dessert(R.drawable.donut, 10, 5),
-        Dessert(R.drawable.eclair, 15, 20),
-        Dessert(R.drawable.froyo, 30, 50),
-        Dessert(R.drawable.gingerbread, 50, 100),
-        Dessert(R.drawable.honeycomb, 100, 200),
-        Dessert(R.drawable.icecreamsandwich, 500, 500),
-        Dessert(R.drawable.jellybean, 1000, 1000),
-        Dessert(R.drawable.kitkat, 2000, 2000),
-        Dessert(R.drawable.lollipop, 3000, 4000),
-        Dessert(R.drawable.marshmallow, 4000, 8000),
-        Dessert(R.drawable.nougat, 5000, 16000),
-        Dessert(R.drawable.oreo, 6000, 20000)
+        Dessert(R.drawable.cupcake, 2, 0),
+        Dessert(R.drawable.donut, 10, 3),
+        Dessert(R.drawable.eclair, 1, 6),
+        Dessert(R.drawable.froyo, 6, 9),
+        Dessert(R.drawable.gingerbread, 7, 12),
+        Dessert(R.drawable.honeycomb, 8, 15),
+        Dessert(R.drawable.icecreamsandwich, 9, 17),
+        Dessert(R.drawable.jellybean, 12, 20),
+        Dessert(R.drawable.kitkat, 15, 23),
+        Dessert(R.drawable.lollipop, 3, 26),
+        Dessert(R.drawable.marshmallow, 2, 29),
+        Dessert(R.drawable.nougat, 10, 32),
+        Dessert(R.drawable.oreo, 5, 35)
     )
 }
 
