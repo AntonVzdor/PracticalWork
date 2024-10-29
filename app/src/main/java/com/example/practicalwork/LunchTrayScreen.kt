493117127f6408d1255951adc91e12439b2f445e
AntonVzdor <der_nom@mail.ru>
2024-10-29 12:15:16 +0300

    Одиннадцатая практическая готова

--- app/src/main/java/com/example/practicalwork/LunchTrayScreen.kt
+++ app/src/main/java/com/example/practicalwork/LunchTrayScreen.kt
@@ -6,15 +6,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.practicalwork.datasource.DataSource
 import com.example.practicalwork.uisreen.AccompanimentMenuScreen
@@ -32,7 +42,29 @@ enum class LunchTrayScreen(@StringRes val title: Int) {
     Checkout(title = R.string.order_checkout)
 }
 
-// TODO: AppBar
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LunchAppBar(
+    lunchTrayScreen: LunchTrayScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    CenterAlignedTopAppBar(
+        title = { Text(text = stringResource(lunchTrayScreen.title)) },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }   
+            }
+        }
+    )
+}
 
 @Composable
 fun LunchTrayApp(
@@ -41,9 +73,18 @@ fun LunchTrayApp(
 
     val viewModel: OrderViewModel = viewModel()
 
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
+    val lunchTrayScreen = LunchTrayScreen.valueOf(
+        backStackEntry?.destination?.route ?: LunchTrayScreen.Start.name
+    )
+
     Scaffold(
         topBar = {
-            // TODO: AppBar
+            LunchAppBar(
+                lunchTrayScreen = lunchTrayScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = {navController.navigateUp()})
         }
     ){innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
@@ -122,14 +163,19 @@ fun LunchTrayApp(
                 CheckoutScreen(
                     orderUiState = uiState,
                     onNextButtonClicked = {
-                        navController.navigate(LunchTrayScreen.Start.name)
+                        navController.popBackStack(LunchTrayScreen.Start.name, inclusive = false)
                     },
                     onCancelButtonClicked = {
-                        navController.navigate(LunchTrayScreen.Start.name)
+                        navController.popBackStack(LunchTrayScreen.Start.name, inclusive = false)
                     },
                     modifier = Modifier
                         .verticalScroll(rememberScrollState())
-                        .padding(innerPadding)
+                        .padding(
+                            top = innerPadding.calculateTopPadding(),
+                            bottom = innerPadding.calculateBottomPadding(),
+                            start = dimensionResource(R.dimen.padding_medium),
+                            end = dimensionResource(R.dimen.padding_medium),
+                        )
                 )
             }
         }
