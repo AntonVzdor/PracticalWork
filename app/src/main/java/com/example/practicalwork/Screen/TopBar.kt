56cfb635f1ab2cb879d85bd681fdc1b6cb03889e
AntonVzdor <135133877+AntonVzdor@users.noreply.github.com>
2025-02-17 15:57:56 +0300

    Начинаю работы над TopBar

--- app/src/main/java/com/example/practicalwork/Screen/TopBar.kt
+++ app/src/main/java/com/example/practicalwork/Screen/TopBar.kt
@@ -6,8 +6,10 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,7 +37,10 @@ fun TopBarApp(
         } else {
             { Box {} }
         },
-        modifier = modifier
+        modifier = modifier,
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
     )
 }
 
