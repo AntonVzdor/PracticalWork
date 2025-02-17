48a63ea1c17a0ee5f48495af2749bae3d6fa1329
AntonVzdor <135133877+AntonVzdor@users.noreply.github.com>
2025-02-17 15:53:04 +0300

    Начинаю работы над TopBar

--- app/src/main/java/com/example/practicalwork/Screen/TopBar.kt
+++ app/src/main/java/com/example/practicalwork/Screen/TopBar.kt
@@ -6,13 +6,10 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.traceEventEnd
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
